a95aa17435b45062af2bb8a54e0c46e3a737a5ed
Henrik.Jurkschat <henrik.jurkschat@ext.one-insurance.com>
2020-03-31 16:54:26 +0200

    display

--- app/src/main/java/com/one/gdvftp/entity/Display.java
+++ app/src/main/java/com/one/gdvftp/entity/Display.java
@@ -1,6 +1,18 @@
 package com.one.gdvftp.entity;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.val;
+
 public interface Display {
+
   /** returns a short string to display the entity */
   String display();
+
+  static <T extends Display> String display(List<T> list) {
+    val displays = list.stream().map(Display::display).collect(Collectors.toList());
+    val s = String.join(", ", displays);
+    return "["+s+"]";
+  }
+
 }
