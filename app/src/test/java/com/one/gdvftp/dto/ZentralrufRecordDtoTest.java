029d3da3f9d0f955f723bc399f1d71781c8cea1f
Henrik.Jurkschat <henrik.jurkschat@ext.one-insurance.com>
2020-04-02 19:25:00 +0200

    more testing for KH,TK,VK

--- app/src/test/java/com/one/gdvftp/dto/ZentralrufRecordDtoTest.java
+++ app/src/test/java/com/one/gdvftp/dto/ZentralrufRecordDtoTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDate;
 import java.util.AbstractMap.SimpleEntry;
+import java.util.Collections;
 import java.util.stream.Stream;
 import lombok.val;
 import org.junit.Test;
@@ -12,19 +13,49 @@ import org.junit.Test;
 public class ZentralrufRecordDtoTest {
 
   @Test
-  public void testRecord() {
+  public void testRecordVK() {
     val date = LocalDate.of(2021,12,30);
     val deductible = Stream.of(new SimpleEntry<>("KH", 0), new SimpleEntry<>("TK", 2345), new SimpleEntry<>("VK", 6789))
         .collect(toMap(SimpleEntry::getKey, SimpleEntry::getValue));
 
     val dto = new ZentralrufRecordDTO(
         123,"vertr1234567890","fakz12345678", date.minusDays(10), date, 12,
-        "KH", true, deductible , 4321, "tsn", date.minusDays(91));
+        "VK", true, deductible , 4321, "tsn", date.minusDays(91));
 
     val rec = dto.toRecord();
     assertThat(rec).isEqualTo(
         "0123"+"vertr1234567890     "+"fakz12345678"+"000"+"20122021"+"30122021"+"012"+
-        "00000000"+"KH"+"1"+"2345"+"6789"+"4321"+"tsn"+"2021");
+        "00000000"+"VK"+"1"+"2345"+"6789"+"4321"+"tsn"+"2021");
+  }
+
+  @Test
+  public void testRecordTK() {
+    val date = LocalDate.of(2021,12,30);
+    val deductible = Stream.of(new SimpleEntry<>("KH", 0), new SimpleEntry<>("TK", 2345))
+        .collect(toMap(SimpleEntry::getKey, SimpleEntry::getValue));
+
+    val dto = new ZentralrufRecordDTO(
+        123,"vertr1234567890","fakz12345678", date.minusDays(10), date, 12,
+        "TK", true, deductible , 4321, "tsn", date.minusDays(91));
+
+    val rec = dto.toRecord();
+    assertThat(rec).isEqualTo(
+        "0123"+"vertr1234567890     "+"fakz12345678"+"000"+"20122021"+"30122021"+"012"+
+            "00000000"+"TK"+"1"+"2345"+"0000"+"4321"+"tsn"+"2021");
+  }
+
+  @Test
+  public void testRecordKH() {
+    val date = LocalDate.of(2021,12,30);
+
+    val dto = new ZentralrufRecordDTO(
+        123,"vertr1234567890","fakz12345678", date.minusDays(10), date, 12,
+        "KH", true, Collections.EMPTY_MAP, 4321, "tsn", date.minusDays(91));
+
+    val rec = dto.toRecord();
+    assertThat(rec).isEqualTo(
+        "0123"+"vertr1234567890     "+"fakz12345678"+"000"+"20122021"+"30122021"+"012"+
+            "00000000"+"KH"+"1"+"0000"+"0000"+"4321"+"tsn"+"2021");
   }
 
 }
